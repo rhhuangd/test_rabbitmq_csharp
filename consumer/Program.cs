4270cfa106d29060a391290c4707bcf1ade4a497
Ryan Huang <ryan.huang@ionetworks.co>
2023-02-16 17:27:08 +0800

    first commit

---
+++ consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// 1. create the connection
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = factory.CreateConnection();
+
+// 2. create channels
+using var channel = connection.CreateModel();
+
+// 3. declare queues
+channel.QueueDeclare(
+    queue: "queue1",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+// 4. create consumers 
+var consumer = new EventingBasicConsumer(channel);
+
+// 5. binding callback functions with message-received events of consumers  
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"message received: {message}");
+};
+
+//6. binding consumers to channels 
+channel.BasicConsume(
+    queue: "queue1", 
+    autoAck: true, 
+    consumer: consumer);
+
+Console.ReadKey();
---
+++ producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+// 1. create the connection
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = factory.CreateConnection();
+
+// 2. create channels
+using var channel = connection.CreateModel();
+
+// 3. declare queues
+channel.QueueDeclare(
+    queue: "queue1", 
+    durable: false, 
+    exclusive: false, 
+    autoDelete: false, 
+    arguments: null);
+
+// 4. create messages
+string msg = "c# first message";
+var message = Encoding.UTF8.GetBytes(msg);
+
+// 5. publish the messages
+channel.BasicPublish(
+    exchange: "", 
+    routingKey: "queue1", 
+    basicProperties: null, 
+    body: message);
+
