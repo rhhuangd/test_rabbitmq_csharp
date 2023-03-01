a279d1d7af9b39b14d7b05f2aed39d29f94494dd
Ryan Huang <ryan.huang@ionetworks.co>
2023-03-01 11:54:53 +0800

    update Request-Reply Pattern

--- 5_Request-Reply/Client/Program.cs
+++ 5_Request-Reply/Client/Program.cs
@@ -1,3 +1,34 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
+// establish connection
+var factory = new ConnectionFactory(){HostName="localhost"};
+var connection = factory.CreateConnection();
+
+// create channel
+var channel = connection.CreateModel();
+
+// set up a consumer for Server replies
+var reply_queue = channel.QueueDeclare();
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (channel, e) => 
+{
+    var body = e.Body.ToArray();
+    var reply = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Reply received: {reply}");
+};
+channel.BasicConsume(reply_queue.QueueName, true, consumer);
+Console.WriteLine("Client - Start Consuming");
+
+// set up a producer for Client requests
+channel.QueueDeclare(queue: "request-queue", exclusive: false, autoDelete: false);
+var request = "This is a client request";
+var body = Encoding.UTF8.GetBytes(request);
+var properties = channel.CreateBasicProperties();
+properties.ReplyTo = reply_queue.QueueName;
+properties.CorrelationId = Guid.NewGuid().ToString();
+channel.BasicPublish("", "request-queue", properties, body);
+Console.WriteLine($"Client - Send request: {properties.CorrelationId}");
+
+Console.ReadKey();
--- 5_Request-Reply/Server/Program.cs
+++ 5_Request-Reply/Server/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// establish connection
+var factory = new ConnectionFactory(){HostName="localhost"};
+var connection = factory.CreateConnection();
+
+// create channel
+var channel = connection.CreateModel();
+
+// set up a consumer for Client requests
+channel.QueueDeclare("request-queue", exclusive: false, autoDelete: false);
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (ch, e) => 
+{
+    Console.WriteLine($"Request received: {e.BasicProperties.CorrelationId}");
+    var reply = $"This is a reply to request: {e.BasicProperties.CorrelationId}";
+    var body = Encoding.UTF8.GetBytes(reply);
+    channel.BasicPublish("", e.BasicProperties.ReplyTo, null, body);
+};
+channel.BasicConsume("request-queue", true, consumer);
+Console.WriteLine("Server - Start Consuming");
+
+Console.ReadKey();
